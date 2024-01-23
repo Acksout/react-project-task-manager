94be76f027f00cf623ca10e9c86f439d17f05334
Acksout <157364563+Acksout@users.noreply.github.com>
2024-01-23 12:04:19 +0530

    Add: Footer with links and svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+// v1 working
+
 import { useState } from "react";
 
 import NewProject from "./components/NewProject.jsx";
@@ -115,15 +117,65 @@ function App() {
   }
 
   return (
-    <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar
-        onStartAddProject={handleStartAddProject}
-        projects={projectsState.projects}
-        onSelectProject={handleSelectProject}
-        selectedProjectId={projectsState.selectedProjectId}
-      />
-      {content}
-    </main>
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-grow flex gap-8">
+        <ProjectsSidebar
+          onStartAddProject={handleStartAddProject}
+          projects={projectsState.projects}
+          onSelectProject={handleSelectProject}
+          selectedProjectId={projectsState.selectedProjectId}
+        />
+        <div className="flex-grow">{content}</div>
+      </main>
+      <footer className="text-center py-4">
+        <div className="flex flex-col items-center justify-center">
+          <h3 className="text-xl mb-2">
+            Made by <span className="font-bold">Rakshit</span>
+          </h3>
+          <div className="flex gap-4">
+            <a
+              href="https://github.com/Acksout"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer"
+            >
+              <img
+                src="https://cdn.discordapp.com/attachments/1199226746902433802/1199226844084445214/github-color-svgrepo-com.svg?ex=65c1c608&is=65af5108&hm=0e6ada272b9959c4f598913f3b4c22f5620034136862b0d5051e116e37437e77&"
+                alt="github-icon"
+                width={35}
+                height={35}
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/rakshitsbb/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer"
+            >
+              <img
+                src="https://cdn.discordapp.com/attachments/1199226746902433802/1199226844638105670/linkedin-icon-svgrepo-com.svg?ex=65c1c608&is=65af5108&hm=ebb8894a41419e8923fc453b0389a00c1e76ae96800d042eb994653ef71f0cec&"
+                alt="linkedin-icon"
+                width={35}
+                height={35}
+              />
+            </a>
+            <a
+              href="https://twitter.com/Rakshit1337"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer"
+            >
+              <img
+                src="https://cdn.discordapp.com/attachments/1199226746902433802/1199226844344487996/twitter-svgrepo-com.svg?ex=65c1c608&is=65af5108&hm=e2ff175f294ab5fe24347cfabbd82b0bdc080a92d1c5bedc100b310478f7fad0&"
+                alt="twitter-icon"
+                width={35}
+                height={35}
+              />
+            </a>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 }
 
